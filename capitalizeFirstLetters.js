d0ccbb0b0b0b9de72f821768641d4afc7bdb6402
pitrouA <adrien.pitrou@gmail.com>
2023-03-22 10:34:41 +0100

    writing function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -7,6 +7,22 @@ assert.strictEqual(capitalizeFirstLetters(''), '');
 function capitalizeFirstLetters(s_input) {
 
   s_strRet = ""
+  b_blankFound = true;
+
+  for(var i=0;i<s_input.length;i++){
+    //uppercase if blank previously
+    if(b_blankFound){
+      s_strRet = s_strRet + s_input.charAt(i).toUpperCase();
+    }else{
+      s_strRet = s_strRet + s_input.charAt(i);
+    }
+    //next uppercase if blank
+    if(s_input.charAt(i) == ' '){
+      b_blankFound = true;
+    }else{
+      b_blankFound = false;
+    }
+  }
 
   return s_strRet;
 }
